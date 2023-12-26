5bc7b4a0beb9bf18f0e3db48dc71c57674ccaca0
Shashank <jshashank454@gmail.com>
2023-12-26 12:56:47 +0530

    day two programs

---
+++ Tns1/src/day2/break_demo.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class break_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i < 10; i++) {
+			if (i == 5)
+				break;
+			System.out.println(i);
+		}
+	}
+}
---
+++ Tns1/src/day2/continue_demo.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class continue_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int k = 5; k < 15; k++)
+		{
+		// Odd numbers are skipped
+		if (k%2 != 0)
+		continue;
+		// Even numbers are printed
+		System.out.print(k + " ");
+		}
+	}	
+}
